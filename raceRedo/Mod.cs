65c5aef4377881fa560d7a884be713e0ecd3887a
Mr__Brick <bardzotak9@gmail.com>
2023-03-21 15:51:06 +0100

    Add project files.

---
+++ raceRedo/Mod.cs
@@ -0,0 +1,20 @@
+﻿using PulsarModLoader;
+
+namespace SimpleCheat
+{
+    public class Mod : PulsarMod
+    {
+        public override string Version => "0.1";
+
+        public override string Author => "Mr__Brick";
+
+        public override string LongDescription => "Lets you replay races for the small price of 100000CR";
+
+        public override string Name => "raceRedo";
+
+        public override string HarmonyIdentifier()
+        {
+            return $"{Author}.{Name}";
+        }
+    }
+}
---
+++ raceRedo/command.cs
@@ -0,0 +1,33 @@
+﻿using PulsarModLoader.Chat.Commands.CommandRouter;
+
+namespace raceRedo
+{
+    internal class command : ChatCommand
+    {
+        public override string[] CommandAliases()
+        {
+            return new string[] { "raceRedo", "redoRaces" };
+        }
+
+        public override string Description()
+        {
+            return "allows to replay races";
+        }
+
+        public override void Execute(string arguments)
+        {
+            if (PLServer.Instance.CurrentCrewCredits >= 100000)
+            {
+                PLServer.Instance.CurrentCrewCredits -= 100000;
+                PLServer.Instance.RacesWonBitfield = 0;
+                PLServer.Instance.RacesLostBitfield = 0;
+                PLServer.Instance.RacesStartedBitfield = 0;
+                PulsarModLoader.Utilities.Messaging.Echo(PhotonTargets.All, "You can now participate in races again!");
+            }
+            else
+            {
+                PulsarModLoader.Utilities.Messaging.Echo(PhotonTargets.All, "You don't have enough credits to participate in races again! Credits required: 100 000 CR");
+            }
+        }
+    }
+}
