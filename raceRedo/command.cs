3f0c22d3aef94b1a01b77ed7877ffdb7c8c7c94e
Mr__Brick <bardzotak9@gmail.com>
2023-03-21 16:54:21 +0100

    Added client-proofing (preventing possible de-syncs)

--- raceRedo/command.cs
+++ raceRedo/command.cs
@@ -16,17 +16,29 @@ namespace raceRedo
 
         public override void Execute(string arguments)
         {
-            if (PLServer.Instance.CurrentCrewCredits >= 100000)
+            if (PhotonNetwork.isMasterClient)
             {
-                PLServer.Instance.CurrentCrewCredits -= 100000;
-                PLServer.Instance.RacesWonBitfield = 0;
-                PLServer.Instance.RacesLostBitfield = 0;
-                PLServer.Instance.RacesStartedBitfield = 0;
-                PulsarModLoader.Utilities.Messaging.Echo(PhotonTargets.All, "You can now participate in races again!");
+                if (PLServer.Instance.CurrentCrewCredits >= 100000)
+                {
+                    PLServer.Instance.CurrentCrewCredits -= 100000;
+                    PLServer.Instance.RacesWonBitfield = 0;
+                    PLServer.Instance.RacesLostBitfield = 0;
+                    PLServer.Instance.RacesStartedBitfield = 0;
+                    PulsarModLoader.Utilities.Messaging.Echo(PhotonTargets.MasterClient,
+                        "You can now participate in races again!");
+                }
+                else
+                {
+                    PulsarModLoader.Utilities.Messaging.Echo(PhotonTargets.MasterClient,
+                        "You don't have enough credits to participate in races again! Credits required: 100 000 CR");
+                }
             }
             else
             {
-                PulsarModLoader.Utilities.Messaging.Echo(PhotonTargets.All, "You don't have enough credits to participate in races again! Credits required: 100 000 CR");
+                PulsarModLoader.Utilities.Messaging.Echo(PhotonTargets.MasterClient,
+                    $"Your crewmate ({PLNetworkManager.GetLocalPlayerName()}) tried using host-only command!");
+                PulsarModLoader.Utilities.Messaging.Notification("You can't use this command! You are not a host!",
+                    PLNetworkManager.Instance.LocalPlayer);
             }
         }
     }
